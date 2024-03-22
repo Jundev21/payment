eefab190e3ffefcc3c7b68bf4d7759ffa3896475
jun <jundev21@gmail.com>
2024-03-22 14:48:09 +0900

    [feat] 프로젝트 세팅

--- user-service/src/main/java/com/userPayment/adapter/in/web/Test.java
+++ user-service/src/main/java/com/userPayment/adapter/in/web/Test.java
@@ -1,9 +1,12 @@
-package com.example.payment;
+package com.userPayment.adapter.in.web;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+//adapter 은 외부와의 직접적인 상호작용을한다.
+//http 요청 이 외부와의 상호작용이다.
 @RestController
 public class Test {
 
